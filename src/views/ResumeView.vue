d364c6905786b642a7f1e9aaade5d53748aa4cb5
Alex <alexandre.rozec@gmail.com>
2023-03-01 13:16:53 +0100

    added an url query to be able to load a context from URL instead of choosing manually

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -11,7 +11,7 @@
 
 <script setup>
 import { onMounted, computed } from 'vue';
-import { useRouter} from 'vue-router';
+import { useRouter, useRoute} from 'vue-router';
 import { useStore } from 'vuex';
 import localStorageService from '@/services/localStorage.service';
 import ResumeIntro from '@/components/resume/ResumeIntro.vue';
@@ -20,6 +20,7 @@ import ExperiencesSection from '@/components/resume/ExperiencesSection.vue';
 import AvailablityBanner from '@/components/common/AvailablityBanner.vue';
 
 const router = useRouter()
+const route = useRoute()
 const store = useStore()
 
 const ctxLoaded = computed(()=> {
@@ -27,22 +28,34 @@ const ctxLoaded = computed(()=> {
 })
 
 onMounted(()=>{
-    const localStorageContext = localStorageService.getResumeContext()
-    console.log(localStorageContext)
+    const resumeCtxQuery = route.query.resumeContext
+    const queryValid = resumeCtxQuery !== undefined && resumeCtxQuery !== ""
+    if(queryValid){
+        setResumeContext(resumeCtxQuery)
+        router.push({name: 'resume'})
+    }else{
+        checkResumeContext()
+    }
+})
+
+const checkResumeContext = () => {
     const storeResumeCtx = store.getters['resume/getContext']
     if(storeResumeCtx === ""){
+        const localStorageContext = localStorageService.getResumeContext()
         if(localStorageContext !== "" && localStorageContext !== null){
-
-            store.dispatch('resume/setContext', localStorageContext)
+            setResumeContext(localStorageContext)
         }else{
             router.push({name: 'chooseJobContext'})
         }
         
 
     }
+}
 
-})
-
+const setResumeContext = (ctx) => {
+    store.dispatch('resume/setContext', ctx)
+}
+ 
 </script>
 
 <style lang="scss">
